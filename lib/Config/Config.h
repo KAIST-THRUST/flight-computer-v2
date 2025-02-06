cc202d8795cb8c6cda32601d3eef4db3df78bc30
Kyungbin Min <128977545+bean0616@users.noreply.github.com>
2025-02-06 16:52:52 +0900

    Add project configuration file (#1)

---
+++ lib/Config/Config.h
@@ -0,0 +1,11 @@
+/**
+ * @file Config.h
+ * @brief File containing main configurations of the project.
+ *
+ */
+
+#pragma once
+
+#define DAQ_NUM_OF_SENSORS 25 // Maximum number of sensors connected.
+#define DAQ_NUM_OF_MUXS 2     // Maximum number of MUXs connected.
+#define DAQ_ADC_RESOLUTION 12 // ADC resolution in bits.
