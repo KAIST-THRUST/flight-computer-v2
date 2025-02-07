6da1c37c50e1dee9ea5a385ca9a62509d57733f1
Kyungbin Min <128977545+bean0616@users.noreply.github.com>
2025-02-07 23:47:00 +0900

    Implement analog sensor library core (#2)
    * Add data structures for analog sensors and multiplexers
    
    * Add AnalogSensorManager class for managing multiple analog sensors

---
+++ lib/AnalogSensor/AnalogSensor.h
@@ -0,0 +1,10 @@
+/**
+ * @file AnalogSensor.h
+ * @brief Library for analog sensor readings and managements.
+ * 
+ */
+
+#pragma once
+
+#include "AnalogSensorTypes.h"
+#include "AnalogSensorManager.h"
