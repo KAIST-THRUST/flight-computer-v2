06f6d3e6fcdacbf06436fc81a97f86e959db1195
Seungeun Yang <62539583+YangPUN@users.noreply.github.com>
2025-02-12 21:44:40 +0900

    Add pressure sensor implementation and psi2bar util function (#10)
    * change power function more efficiently
    
    * revise power function
    
    * add psi2bar function
    
    * add psi2bar function
    
    * add pressuresensor getSensorData function
    
    * move variable DAQ_ADC_MAX to config.h, change power function of Utils.cpp

--- lib/AnalogSensor/Implementations/PressureSensor.cpp
+++ lib/AnalogSensor/Implementations/PressureSensor.cpp
@@ -1,11 +1,17 @@
 #include "PressureSensor.h"
 
-const double LowRangePressureSensor::getSensorData(int raw_data) const {
+const double LowRangePressureSensor::getSensorData(int raw_data) const
+{
   /* TODO: Implement the conversion from raw data to pressure for the low
   range sensor. */
+  double LowRangePressure_result = static_cast<double>(raw_data) / static_cast<double>(DAQ_ADC_MAX) * psi2bar(1000);
+  return LowRangePressure_result;
 }
 
-const double HighRangePressureSensor::getSensorData(int raw_data) const {
+const double HighRangePressureSensor::getSensorData(int raw_data) const
+{
   /* TODO: Implement the conversion from raw data to pressure for the high
   range sensor. */
+  double HighRangePressure_result = static_cast<double>(raw_data) / static_cast<double>(DAQ_ADC_MAX) * psi2bar(2000);
+  return HighRangePressure_result;
 }
--- lib/Utils/Utils.cpp
+++ lib/Utils/Utils.cpp
@@ -1,11 +1,14 @@
 #include "Utils.h"
 
+// power function.
 int power(int base, int exponent)
 {
-    int result = 1;
-    for (int i = 0; i < exponent; i++)
-    {
-        result = result * base;
-    }
-    return result;
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result = result * base;
+        }
+        return result;
 }
+
+double psi2bar(double pressure_data){ return pressure_data/14.503773773; }
