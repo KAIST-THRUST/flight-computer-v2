aaf519325e3111ab704fd01bf6ff8dd76776f7c5
Minho Yang <119037177+kaistminho@users.noreply.github.com>
2025-02-12 05:04:26 +0000

    Add-pressuresensor-getsensordata-fucntion

--- lib/AnalogSensor/Implementations/PressureSensor.cpp
+++ lib/AnalogSensor/Implementations/PressureSensor.cpp
@@ -3,9 +3,17 @@
 const double LowRangePressureSensor::getSensorData(int raw_data) const {
   /* TODO: Implement the conversion from raw data to pressure for the low
   range sensor. */
+  double LowRangePressureSensor_result = 0;
+  int scope = power(2, DAQ_ADC_RESOLUTION);
+  LowRangePressureSensor_result=raw_data*(70/scope);
+  return LowRangePressureSensor_result;
 }
 
 const double HighRangePressureSensor::getSensorData(int raw_data) const {
   /* TODO: Implement the conversion from raw data to pressure for the high
   range sensor. */
+  double HighRangePressureSensor_result = 0;
+  int scope = power(2, DAQ_ADC_RESOLUTION);
+  HighRangePressureSensor_result=raw_data*(138/scope);
+  return HighRangePressureSensor_result;
 }
