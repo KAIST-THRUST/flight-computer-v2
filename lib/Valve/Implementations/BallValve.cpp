e40cc6f4197f8bf61f290ce1d4de32292ae8a0fc
jong heun <justdeep84@gmail.com>
2025-04-23 22:57:54 +0900

    i added servo offset so no gulijuk

--- lib/Valve/Implementations/BallValve.cpp
+++ lib/Valve/Implementations/BallValve.cpp
@@ -5,29 +5,30 @@
 BallValve::BallValve(uint8_t BallValve_pin_num) : pin_num(BallValve_pin_num)    {}
 
 
-void BallValve::begin() {
+void BallValve::begin(int offset_setting) {
     servo.attach(pin_num,500,2500); //PWM pulse width range for https://www.amazon.com/Torque-Motors-Waterproof-Steering-Control/dp/B073F92G2S?th=1
-    servo.write(0);
+    offset=offset_setting;
+    servo.write(0+offset);
 }
 
 void BallValve::open() {
-    servo.write(90);
+    servo.write(90+offset);
 
 }
 void BallValve::halfOpen() {
-    servo.write(45);
+    servo.write(45+offset);
 }
 
 void BallValve::close() {
-    servo.write(0);
+    servo.write(0+offset);
 }
 
 
 void BallValve::open(int angle) {
-    servo.write(angle);
+    servo.write(angle+offset);
 }
 
 
 int BallValve::showAngle() {
-    return servo.read();
+    return servo.read()-offset;
 }
--- lib/Valve/Implementations/BallValve.h
+++ lib/Valve/Implementations/BallValve.h
@@ -9,10 +9,11 @@ class BallValve{
     private:
     Servo servo;
     const uint8_t pin_num;
+    int offset;
 
     public:
     BallValve(uint8_t BallValve_pin_num);
-    void begin();   //set servo pin and initialize angle to 0 degrees
+    void begin(int offset);   //set servo pin and initialize angle to 0 degrees
 
     void open();    //servo to 90 degrees
     void halfOpen();    //servo to 45 degrees
