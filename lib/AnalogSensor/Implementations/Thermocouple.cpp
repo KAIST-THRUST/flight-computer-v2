f5668b24be7658711fd1236c5359073efb8b0e4c
Yong Lin <yonglin2902@gmail.com>
2025-04-23 22:36:34 +0900

    Calibrated voltage for temperature sensor (#24)

--- lib/AnalogSensor/Implementations/Thermocouple.cpp
+++ lib/AnalogSensor/Implementations/Thermocouple.cpp
@@ -3,13 +3,15 @@
 // K Type Thermocouple for 0 to 1000 degree
 const double KTypeThermocoupleHigh::getSensorData(int raw_data) const {
   double voltage = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
-  double temperature = map(voltage, 0.0, 3.3, 0.0, 1000.0);
+  voltage *= 2; // Calibration
+  double temperature = map(voltage, 0.0, 5.0, 0.0, 1000.0);
   return temperature;
 }
 
 // K Type Thermocouple for -250 to 750 degree
 const double KTypeThermocoupleLow::getSensorData(int raw_data) const {
   double voltage = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
-  double temperature = map(voltage, 0.0, 3.3, -250.0, 750.0);
+  voltage *= 2; // Calibration
+  double temperature = map(voltage, 0.0, 5.0, -250.0, 750.0);
   return temperature;
 }
