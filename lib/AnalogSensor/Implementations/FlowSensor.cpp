45ba7c214abee5241ed8160979f5eed212bb9c75
aeroazetro <israelpenalosa@kaist.ac.kr>
2025-05-07 22:54:52 +0900

    Add resistor variables for analog sensors calculation (#32)
    Co-authored-by: israelpenalosa <israelpenalosa@obf.ateneo.edu>

--- lib/AnalogSensor/Implementations/FlowSensor.cpp
+++ lib/AnalogSensor/Implementations/FlowSensor.cpp
@@ -1,15 +1,16 @@
 #include "FlowSensor.h"
+#include "../Config/Config.h"
 
 const double FlowSensorHigh::getSensorData(int raw_data) const {
     double voltage = map((double)raw_data, 0, DAQ_ADC_MAX, 0.0, 3.3);
-    double current = voltage / 150;
+    double current = voltage / VOLTAGE_DIVIDER_R4;
     double flowrate = map(current, 0.004, 0.020, 0.0, 3000); // in kg/h
     return flowrate;
 }
 
 const double FlowSensorLow::getSensorData(int raw_data) const {
     double voltage = map((double)raw_data, 0, DAQ_ADC_MAX, 0.0, 3.3);
-    double current = voltage / 150;
+    double current = voltage / VOLTAGE_DIVIDER_R4;;
     double flowrate = map(current, 0.004, 0.020, 0.0, 1200); // in kg/h
     return flowrate;
 }
