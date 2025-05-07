7da5878dcc8600ae8811e4267f121cae12735917
israelpenalosa <israelpenalosa@obf.ateneo.edu>
2025-05-07 22:20:37 +0900

    initialize resistor variables for thermo, pressure, and flow

--- lib/AnalogSensor/Implementations/PressureSensor.cpp
+++ lib/AnalogSensor/Implementations/PressureSensor.cpp
@@ -1,15 +1,28 @@
 #include "PressureSensor.h"
+#include "../Config/Config.h"
 
 const double LowRangePressureSensor::getSensorData(int raw_data) const {
   double voltage = map((double)raw_data, 0, DAQ_ADC_MAX, 0.0, 3.3);
-  double current = voltage / 150.0;
+  double current = voltage / VOLTAGE_DIVIDER_R4;;
   double pressure = map(current, 0.004, 0.020, 0.0, 1000);
   return psi2bar(pressure);
 }
 
 const double HighRangePressureSensor::getSensorData(int raw_data) const {
   double voltage = map((double)raw_data, 0, DAQ_ADC_MAX, 0.0, 3.3);
-  double current = voltage / 150.0;
+  double current = voltage / VOLTAGE_DIVIDER_R4;;
   double pressure = map(current, 0.004, 0.020, 0.0, 2000);
   return psi2bar(pressure);
 }
+
+
+/* 
+
+  flow R4
+
+  pressure R4
+
+  thermo R1 + R2. but read from R1
+
+
+*/
--- lib/AnalogSensor/Implementations/Thermocouple.cpp
+++ lib/AnalogSensor/Implementations/Thermocouple.cpp
@@ -1,17 +1,22 @@
 #include "Thermocouple.h"
+#include "../Config/Config.h"
 
 // K Type Thermocouple for 0 to 1000 degree
 const double KTypeThermocoupleHigh::getSensorData(int raw_data) const {
-  double voltage = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
-  voltage *= 2; // Calibration
-  double temperature = map(voltage, 0.0, 5.0, 0.0, 1000.0);
+  double v_out = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
+
+  double v_in = v_out * ((VOLTAGE_DIVIDER_R1 + VOLTAGE_DIVIDER_R2) / (double)VOLTAGE_DIVIDER_R2);
+
+  double temperature = map(v_in, 0.0, 5.0, 0.0, 1000.0);
   return temperature;
 }
 
 // K Type Thermocouple for -250 to 750 degree
 const double KTypeThermocoupleLow::getSensorData(int raw_data) const {
-  double voltage = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
-  voltage *= 2; // Calibration
-  double temperature = map(voltage, 0.0, 5.0, -250.0, 750.0);
+  double v_out = ((double)raw_data / DAQ_ADC_MAX) * 3.3;
+  
+  double v_in = v_out * ((VOLTAGE_DIVIDER_R1 + VOLTAGE_DIVIDER_R2) / (double)VOLTAGE_DIVIDER_R2);
+
+  double temperature = map(v_in, 0.0, 5.0, -250.0, 750.0);
   return temperature;
 }
