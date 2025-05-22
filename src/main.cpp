b2cad418186a60f601802c6f63d802b9eeae3886
Kyungbin Min <kbmin2711@gmail.com>
2025-05-23 01:12:51 +0900

    Add command execution functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // A simple example of testing each unit of the DAQ.
 
 #include <AnalogSensor.h>
+#include <CommandParser.h>
 #include <Config.h>
 #include <Logger.h>
 #include <Valve.h>
@@ -15,6 +16,10 @@ HighRangePressureSensor pt2(&mux1, 1);
 AnalogSensorManager sensor_manager;
 BallValve valve(8);
 SolenoidValve sv(28);
+CommandParser command_parser;
+char command_buffer[COMMAND_MESSAGE_SIZE];
+
+void excuteCommand(Command &command);
 
 void setup() {
   Serial.begin(115200);
@@ -28,34 +33,55 @@ void setup() {
   valve.begin();
   sv.begin();
   delay(2000);
+  Serial.setTimeout(0);
 }
 
 void loop() {
-  // Analog sensor test.
+  /* Control test. */
+  if (Serial.readBytes(command_buffer, COMMAND_MESSAGE_SIZE) == COMMAND_MESSAGE_SIZE) {
+    struct Command command = command_parser.parse(command_buffer);
+    excuteCommand(command);
+  }
+
+  /* Analog sensor test. */
   sensor_manager.updateSensorData(); // Fetches sensor data from the sensors.
   const double *data_arr = sensor_manager.getSensorData();
-  Serial.print("PT1: ");
-  Serial.print(data_arr[0]);
-  Serial.print(" PT2: ");
-  Serial.print(data_arr[1]);
-  Serial.print(" TT1: ");
-  Serial.print(data_arr[2]);
-  Serial.print(" TT2: ");
-  Serial.println(data_arr[3]);
+  logger.logBinary(LogType::DATA, millis(), (byte *)data_arr, sizeof(double) * 4,
+                   LogStream::SERIAL_MAIN);
 
-  // Ball valve test.
-  valve.open(0);
-  Serial.println(valve.showAngle());
-  delay(2000);
-  valve.open(90);
-  Serial.println(valve.showAngle());
-  delay(2000);
+  /* For human readable output, uncomment the following lines. */
+  // Serial.print("PT1: ");
+  // Serial.print(data_arr[0]);
+  // Serial.print(" PT2: ");
+  // Serial.print(data_arr[1]);
+  // Serial.print(" TT1: ");
+  // Serial.print(data_arr[2]);
+  // Serial.print(" TT2: ");
+  // Serial.println(data_arr[3]);
+}
 
-  // Solenoid valve test.
-  sv.open();
-  Serial.println(sv.isOpen() ? "Open" : "Closed");
-  delay(500);
-  sv.close();
-  Serial.println(sv.isOpen() ? "Open" : "Closed");
-  delay(500);
+void excuteCommand(Command &command) {
+  if (command.is_valid) {
+    switch (command.type) {
+    case CommandType::BALL_VALVE_CONTROL:
+      if (command.valve_position >= 0 && command.valve_position <= 90) {
+        valve.open(command.valve_position);
+      }
+      break;
+    case CommandType::SOLENOID_VALVE_CONTROL:
+      if (command.valve_position == 0) {
+        sv.close();
+      } else if (command.valve_position == 1) {
+        sv.open();
+      }
+      break;
+    case CommandType::EMERGENCY_STOP:
+      sv.close();
+      valve.close();
+      break;
+    default:
+      logger.log(LogType::ERROR, millis(), "Invalid command type", LogStream::SERIAL_MAIN);
+      break;
+    }
+  }
 }
